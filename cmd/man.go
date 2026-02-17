053e10338f27abda06c97e4538a6463af8874c71
Polshkrev <105235054+Polshkrev@users.noreply.github.com>
2026-02-17 04:09:27 -0500

    Update man.go

--- cmd/man.go
+++ cmd/man.go
@@ -7,8 +7,9 @@ import (
 	"path/filepath"
 
 	"github.com/Polshkrev/gopolutils"
+	"github.com/Polshkrev/gopolutils/collections"
+	"github.com/Polshkrev/gopolutils/collections/safe"
 	"github.com/Polshkrev/gopolutils/fayl"
-	"github.com/Polshkrev/goserialize"
 	"github.com/Polshkrev/man"
 )
 
@@ -39,7 +40,7 @@ func getArgument(index, minimum, maximum uint8, args ...string) (string, *gopolu
 }
 
 // Read content into a result parametre from a given path constructed from its root and two isometric children based on a sentinal boolean flag.
-func readFiles(read bool, documentationFolder, manualsFolder string, result *goserialize.Object) {
+func readFiles(read bool, documentationFolder, manualsFolder string, result *collections.View[man.Page]) {
 	if !read {
 		return
 	}
@@ -48,13 +49,13 @@ func readFiles(read bool, documentationFolder, manualsFolder string, result *gos
 }
 
 // Write given content to a given path based on a sentinal boolean flag.
-func writeFiles(write bool, target *fayl.Path, content *goserialize.Object) {
+func writeFiles(write bool, target *fayl.Path, content collections.View[man.Page]) {
 	if !write {
 		return
 	} else if content == nil {
 		return
 	}
-	var except *gopolutils.Exception = fayl.WriteObject(target, content)
+	var except *gopolutils.Exception = fayl.WriteList(target, content)
 	if except != nil {
 		panic(except)
 	}
@@ -64,13 +65,6 @@ func appendRoot(root *fayl.Path, child string) string {
 	return filepath.Join(root.ToString(), string(filepath.Separator), child)
 }
 
-// func If[TrueValue, FalseValue any](condition bool, trueValue TrueValue, falseValue FalseValue) {
-// 	if !condition {
-// 		return falseValue
-// 	}
-// 	return trueValue
-// }
-
 func getTargetFile(name string, fileType fayl.Suffix) string {
 	var root *fayl.Path = getRoot()
 	var documentationPath *fayl.Path = fayl.PathFrom(appendRoot(root, documentationFolder))
@@ -78,22 +72,30 @@ func getTargetFile(name string, fileType fayl.Suffix) string {
 	return fayl.PathFromParts(manualsPath.ToString(), name, fileType).ToString()
 }
 
+func find(name string, section man.Section, entries collections.View[man.Page]) man.Page {
+	if section == man.None {
+		return gopolutils.Must(man.FindByTitle(entries, name))
+	}
+	return gopolutils.Must(man.FindByNameFromSection(entries, name, section))
+}
+
 func main() {
 	var write *bool = flag.Bool("write", false, "Write the in-memory cache to a persistant target file.")
 	var read *bool = flag.Bool("read", false, "Read files into the in-memory cache")
 	var target *string = flag.String("o", getTargetFile("pages", fayl.Json), "Output file to dump the in-memory cache. This will only matter if the 'read' flag is set.")
 	var size *bool = flag.Bool("n", false, "Print the total amount of pages.")
+	var section *string = flag.String("s", man.None, "Specify the section from which to lookup.")
 	flag.Parse()
 	var targetPath *fayl.Path = fayl.PathFrom(*target)
-	var data *goserialize.Object = new(goserialize.Object)
-	readFiles(*read, documentationFolder, manualsFolder, data)
+	var data collections.View[man.Page] = safe.NewArray[man.Page]()
+	readFiles(*read, documentationFolder, manualsFolder, &data)
 	writeFiles(*write, targetPath, data)
-	data = gopolutils.Must(fayl.ReadObject[goserialize.Object](targetPath))
+	data = gopolutils.Must(fayl.ReadList[man.Page](targetPath))
 	if *size {
 		fmt.Println(len(*data))
 		os.Exit(0)
 	}
 	var name string = gopolutils.Must(getArgument(0, minimumArgumentCount, maximumArgumentCount, flag.Args()...))
-	var content string = gopolutils.Must(man.FindByTitle(*data, name))
-	fmt.Print(content)
+	var page man.Page = find(name, man.Section(*section), data)
+	fmt.Print(page.Content)
 }
